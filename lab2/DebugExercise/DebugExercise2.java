76bfe9a90ff02b690efc6753daa88298b153df98
ba-king <353843@qq.com>
2026-07-12 14:21:08 +0800

    Adding leecture code and Finish lab2

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -7,32 +7,38 @@ package DebugExercise;
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. */
     public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
+//        int w = (b - a) >> 31;
+//        /* If you're stepping into this function, click the
+//           step out button because you're not going to learn anything. */
+//        int z = ~(b - a) >> 31;
+//
+//        int max = b & w | a & z;
+//        return max;
+        if ((a - b) >= 0) {
+            return a;
 
-        int max = b & w | a & z;
-        return max;
+        }
+        return b;
     }
 
 
     /** Returns the sum of a and b. Do not step into this function. */
     public static int add(int a, int b) {
-        int x = a, y = b;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int xor, and, temp;
-        and = x & y;
-        xor = x ^ y;
+//        int x = a, y = b;
+//        /* If you're stepping into this function, click the
+//           step out button because you're not going to learn anything. */
+//        int xor, and, temp;
+//        and = x & y;
+//        xor = x ^ y;
+//
+//        while (and != 0) {
+//            and <<= 1;
+//            temp = xor ^ and;
+//            and &= xor;
+//            xor = temp;
+//        }
+        return a + b;
 
-        while (and != 0) {
-            and <<= 1;
-            temp = xor ^ and;
-            and &= xor;
-            xor = temp;
-        }
-        return xor;
     }
 
     /** Returns a new array where entry i is the max of
@@ -58,7 +64,8 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+//            sum = sum + add(sum, x[i]);
+            sum = add(sum, x[i]);
             i = i + 1;
         }
         return sum;
--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -14,7 +14,11 @@ public class IntListExercises {
             head.first += c;
             head = head.rest;
         }
+        if (head.rest == null) {
+            head.first += c;
+        }
     }
+
 
     /**
      * Part B: Buggy method that sets node.first to zero if
@@ -26,7 +30,9 @@ public class IntListExercises {
     public static void setToZeroIfMaxFEL(IntList L) {
         IntList p = L;
         while (p != null) {
-            if (firstDigitEqualsLastDigit(max(p))) {
+            int currentMax = max(p);
+            boolean firstEqualsLast = firstDigitEqualsLastDigit(currentMax);
+            if (firstEqualsLast) {
                 p.first = 0;
             }
             p = p.rest;
@@ -51,10 +57,10 @@ public class IntListExercises {
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
-        int firstDigit = x % 10;
+        int firstDigit = x;
         return firstDigit == lastDigit;
     }
 
@@ -76,7 +82,10 @@ public class IntListExercises {
         if (currElemIsPrime) {
             lst.first *= lst.first;
         }
+        boolean restHasPrime = squarePrimes(lst.rest);
 
-        return currElemIsPrime || squarePrimes(lst.rest);
+        return currElemIsPrime || restHasPrime;
+
+//        return currElemIsPrime || squarePrimes(lst.rest);
     }
 }
